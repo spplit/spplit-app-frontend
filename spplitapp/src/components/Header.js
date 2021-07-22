9ed1009060c150cfcaa89ba5537c3302daea3d05
Sukyeong Park <zztnrudzz123433@gmail.com>
2021-07-22 18:55:42 +0900

    Font and Image

--- spplitapp/src/components/Header.js
+++ spplitapp/src/components/Header.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import styled from 'styled-components/native';
+import * as Font from 'expo-font';
 
 export default function Header() {
 
