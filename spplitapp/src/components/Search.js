e73ea9e99063b403cf1df356a787da3deda421cb
Sukyeong Park <zztnrudzz123433@gmail.com>
2021-07-23 23:42:35 +0900

    Initial Searchbar

--- spplitapp/src/components/Search.js
+++ spplitapp/src/components/Search.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import styled from 'styled-components/native';
 import * as Font from 'expo-font';
 
+const SearchbarContainer = styled.View`
+    flex: 0.07;
+    /* background-color: aliceblue; */
+    justify-content: center;
+    align-items: center;
+`;
+
+const Searchbar = styled.TextInput `
+    width: 88%;
+    height: 70%;
+    background-color: #f2f2f2;
+    border-radius: 7px;
+    padding-left: 15px;
+    font-size: 20px;
+`;
+
 export default function Search() {
+    const [text, setText] = useState('');
     return (
-        <View />
+        <SearchbarContainer>
+            <Searchbar />
+        </SearchbarContainer>
     )
 }
--- spplitapp/src/routes/Main.js
+++ spplitapp/src/routes/Main.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from '../components/Header';
+import styled from 'styled-components/native';
+import Search from '../components/Search';
+
+const MainView = styled.View `
+    flex: 1;
+`;
+
 
 export default function Main() {
     return(
+        <MainView>
             <Header />
+            <Search />
+        </MainView>
     )
 }
