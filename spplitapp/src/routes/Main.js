6b15803fa5706b0de7fbb1d9674735b8ee35205c
Sanghyo <sanghyoboy@naver.com>
2021-08-09 17:52:34 +0900

    qr modal

--- spplitapp/src/routes/Main.js
+++ spplitapp/src/routes/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import Header from '../components/Header';
 import styled from 'styled-components/native';
@@ -6,19 +7,22 @@ import Search from '../components/Search';
 import Category from '../components/Category';
 import NameCard from '../components/NameCard';
 import FloatingQR from '../components/FloatingQR';
+import QRModal from '../components/QRModal';
 import { useNavigation } from '@react-navigation/native';
 
-const MainView = styled.View `
+const MainView = styled.View`
     flex: 1;
 `;
 
 const Main = () => {
+    const [visible, setVisible] = useState(false);
 
-    return(
+    return (
         <MainView>
             <Header />
             <Category />
-            <FloatingQR />
+            <FloatingQR setVisible={setVisible} />
+            <QRModal visible={visible} setVisible={setVisible} />
         </MainView>
     )
 }
