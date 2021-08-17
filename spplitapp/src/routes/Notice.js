de078fd6c3d4893cb128187ff30371d26df76817
Sanghyo <sanghyoboy@naver.com>
2021-08-17 20:45:09 +0900

    card request accept, decline success

--- spplitapp/src/routes/Notice.js
+++ spplitapp/src/routes/Notice.js
@@ -1,47 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import {
     StyleSheet,
     Text,
     View,
-    Button,
-    Animated,
-    TouchableOpacity,
-    TouchableHighlight,
-    StatusBar,
-    Touchable,
+    TouchableOpacity
 } from 'react-native';
 import axios from 'axios';
-import { SwipeListView } from 'react-native-swipe-list-view';
-import { useAnimatedStyle } from 'react-native-reanimated';
 import { useNavigation } from '@react-navigation/native';
+import { useIsFocused } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
-const Notifications = [
-    {
-        id: 1,
-        title: "New Request from Hyeonmin Park"
-    },
-    {
-        id: 2,
-        title: "There are 3 Namecards you never used"
-    },
-    {
-        id: 3,
-        title: "Check updated Spplit!"
-    },
-    {
-        id: 4,
-        title: "New Namecard added in your wallet"
-    },
-    {
-        id: 5,
-        title: "New Request from Hyebin Lee"
-    },
-    {
-        id: 6,
-        title: "Pizza Event: Share in Instagram"
-    }
-]
 
 const backbutton = require('../assets/images/backbutton_icon.png')
 
@@ -83,7 +52,9 @@ const NoticeOuterContainer = styled.View`
 `;
 
 const NoticeCardContainer = styled.View`
+    flex-direction: row;
     justify-content: center;
+    align-items: center;
     padding-left: 10px;
     padding-right: 10px;
     width: 95%;
@@ -94,16 +65,48 @@ const NoticeCardContainer = styled.View`
     box-shadow: 1.95px 1.95px 2.6px rgba(0, 0, 0, 0.15) ;
 `;
 
-const Notice = ({ navigation }) => {
+const NoticeAppointContainer = styled.TouchableOpacity`
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding-left: 10px;
+    padding-right: 10px;
+    width: 95%;
+    height: 50px;
+    margin: 10px;
+    background-color: white;
+    border-radius: 10px;
+    box-shadow: 1.95px 1.95px 2.6px rgba(0, 0, 0, 0.15) ;
+`;
+const CardText = styled.View`
+    width: 80%;
+`;
+
+const CardIcons = styled.View`
+    width: 20%;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+`;
+const CardIcon = styled.View`
+    width: 25;
+`;
+
+
+export default function Notice({ navigation }) {
+    const USER_TOKEN = "57c05ec10b751d982859426d129b2553d78fc5c1"
+    const AuthStr = "Token ".concat(USER_TOKEN)
     const cardurl = "http://spplitsuccess.eba-xefre73m.us-west-2.elasticbeanstalk.com/request";
     const appointurl = "http://spplitsuccess.eba-xefre73m.us-west-2.elasticbeanstalk.com/appointment/request";
+    const userurl = "http://spplitsuccess.eba-xefre73m.us-west-2.elasticbeanstalk.com/user"
     const [cards, setCards] = useState([]);
     const [appointments, setAppointments] = useState([]);
+    const [username, setUsername] = useState();
     const [isLoading1, setLoading1] = useState(true);
     const [isLoading2, setLoading2] = useState(true);
+    const [isLoading3, setLoading3] = useState(true);
+    const isFocused = useIsFocused();
     useEffect(() => {
-        const USER_TOKEN = "57c05ec10b751d982859426d129b2553d78fc5c1"
-        const AuthStr = "Token ".concat(USER_TOKEN)
         console.log(AuthStr)
         axios.get(cardurl, { headers: { Authorization: AuthStr } })
             .then((response) => {
@@ -127,78 +130,58 @@ const Notice = ({ navigation }) => {
                 console.log(error)
                 console.log("category loading failure");
             })
-    }, [])
-    const [listData, setListData] = useState(
-        Notifications.map((NotificationItem, index) => ({
-            key: `${index}`,
-            title: NotificationItem.title,
-        }))
-    );
+        axios.get(userurl, { headers: { Authorization: AuthStr } })
+            .then((response) => {
+                console.log("user loading success")
+                console.log(response.data)
+                setUsername(response.data[0].username)
+            })
+            .finally(() => setLoading3(false))
+            .catch((error) => {
+                console.log(error)
+                console.log("user loading failure");
+            })
+    }, [isFocused])
 
-    const closerow = (rowMap, rowKey) => {
-
+    const acceptCard = (pk) => {
+        const url = `http://spplitsuccess.eba-xefre73m.us-west-2.elasticbeanstalk.com/request/${pk}/accept`
+        axios.get(url, { headers: { Authorization: AuthStr } })
+            .then((response) => {
+                console.log("accept success")
+            })
     }
 
-    const deleterow = (rowMap, rowKey) => {
-
+    const declineCard = (pk) => {
+        const url = `http://spplitsuccess.eba-xefre73m.us-west-2.elasticbeanstalk.com/request/${pk}/decline`
+        axios.get(url, { headers: { Authorization: AuthStr } })
+            .then((response) => {
+                console.log("decline success")
+            })
     }
 
-    const VisibleItem = props => {
-        const { data } = props;
-
-        return (
-            <View>
-                <TouchableHighlight>
-                    <NoticeCardContainer>
-                        <Text numberOfLines={1}>{data.item.title}</Text>
-                    </NoticeCardContainer>
-                </TouchableHighlight>
-            </View>
-        );
-    };
-
-    const renderItem = (data) => {
-        return (
-            <VisibleItem data={data} />
-        );
-    };
-
-    const HiddenItemWithActions = props => {
-        const { onClose, onDelete } = props;
-
-        return (
-            <View>
-                <Text>left</Text>
-                <TouchableOpacity>
-                    <Text>Close</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>Delete</Text>
-                </TouchableOpacity>
-            </View>
-        );
-    }
-
-    const renderHiddenItem = (data, rowMap) => {
-        <HiddenItemWithActions
-            data={data}
-            rowMap={rowMap}
-            onClose={() => closeRow(rowMap, data.item.key)}
-            onDelete={() => deleteRow(rowMap, data.item.key)}
-        />
-    };
-
-    const CardRequests = cards.map((request, index) => {
+    const cardRequests = cards.filter((request, index) => request.sender !== username).map((val, index) => {
         return <NoticeCardContainer>
-            <Text>{request.sender} sent you a namecard request</Text>
+            <CardText>
+                <Text numberOfLines={1}>{val.sender} 님께서 명함을 요청하였습니다.</Text>
+            </CardText>
+            <CardIcons>
+                <TouchableOpacity onPress={() => acceptCard(val.pk)}><Icon name="check" size={25} /></TouchableOpacity>
+                <TouchableOpacity onPress={() => declineCard(val.pk)}><Icon name="x" size={25} /></TouchableOpacity>
+            </CardIcons>
         </NoticeCardContainer>
     })
 
 
-    const AppointRequests = appointments.map((request, index) => {
-        return <NoticeCardContainer>
-            <Text>{request.sender_name} sent you an appointment request</Text>
-        </NoticeCardContainer>
+    const appointRequests = appointments.filter((request, index) => request.sender_name !== username).map((val, index) => {
+        return <NoticeAppointContainer>
+            <CardText>
+                <Text numberOfLines={1}>{val.sender_name} 님께서 약속 요청을 보내셨습니다.</Text>
+            </CardText>
+            <CardIcons>
+                <CardIcon></CardIcon>
+                <Icon name="chevron-right" size={25} />
+            </CardIcons>
+        </NoticeAppointContainer>
     })
 
     return (
@@ -214,15 +197,8 @@ const Notice = ({ navigation }) => {
             <NoticeOuterContainer>
                 {isLoading1 && isLoading2 ? <Text>isLoading</Text> :
                     <>
-                        <CardRequests />
-                        <AppointRequests />
-                        {/* <SwipeListView
-                    data={listData}
-                    renderItem={renderItem}
-                    renderHiddenItem={renderHiddenItem}
-                    leftOpenValue={75}
-                    rightOpenValue={-150}
-                /> */}
+                        {cardRequests}
+                        {appointRequests}
                     </>
                 }
             </NoticeOuterContainer>
@@ -230,5 +206,3 @@ const Notice = ({ navigation }) => {
 
     )
 }
-
-export default Notice;
